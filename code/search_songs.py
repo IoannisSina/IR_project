37aad527b499cacdfd5ef1ec780879236eab8b71
kounelisagis <kounelisagis@gmail.com>
2020-11-15 14:51:01 +0200

    song attributes extended

--- code/search_songs.py
+++ code/search_songs.py
@@ -3,16 +3,32 @@ import pandas as pd
 from pandarallel import pandarallel
 import API_keys
 
-api_url = 'https://api.genius.com/search?q='
+api_search_url = 'https://api.genius.com/search?q='
+api_songs_url = 'https://api.genius.com/songs/'
+
 headers = {'Authorization': API_keys.genius_key }
+
 def search_genius(x):
 
     try:
-        r = requests.get(api_url + x.track_name + ' ' + x.artist_name, headers=headers)
+        r = requests.get(api_search_url + x.track_name + ' ' + x.artist_name, headers=headers)
         result = r.json()['response']['hits'][0]['result']
 
+        genius_track_id = result['id']
+
+        r = requests.get(api_songs_url + str(genius_track_id), headers=headers)
+        song = r.json()['response']['song']
+
+        for dictionary in song['media']:
+            provider = dictionary['provider']
+            if provider != 'spotify':
+                x[provider] = dictionary['url']
+
+        x['songwriter_ids'] = [dictionary['id'] for dictionary in song['writer_artists']]
+        x['producer_ids'] = [dictionary['id'] for dictionary in song['producer_artists']]
+
+        x['genius_track_id'] = genius_track_id
         x['annotation_count'] = result['annotation_count']
-        x['song_id'] = result['id']
         x['pyongs'] = result['pyongs_count']
         x['lyrics_owner_id'] = result['lyrics_owner_id']
         x['hot'] = result['stats']['hot']
@@ -21,20 +37,20 @@ def search_genius(x):
         artist_name = result['primary_artist']['name']
 
         if '&' in artist_name:
-            artist_ids = []
+            singer_ids = []
             names = [name.strip() for name in artist_name.split('&')]
             for name in names:
-                r = requests.get(api_url + name, headers=headers)
+                r = requests.get(api_search_url + name, headers=headers)
                 hits = r.json()['response']['hits']
                 for hit in hits:
                     if hit['result']['primary_artist']['name'] == name:
-                        artist_ids.append(hit['result']['primary_artist']['id'])
+                        singer_ids.append(hit['result']['primary_artist']['id'])
                         break
             
-            x['artist_ids'] = artist_ids
+            x['singer_ids'] = singer_ids
             return x
 
-        x['artist_ids'] = [result['primary_artist']['id'] if 'id' in result['primary_artist'] else None]
+        x['singer_ids'] = [result['primary_artist']['id'] if 'id' in result['primary_artist'] else None]
 
     except requests.exceptions.RequestException as e:
         # print(e)
@@ -47,14 +63,14 @@ if __name__ == '__main__':
 
     pandarallel.initialize()
 
-    df = pd.read_csv('dataset.csv')
+    df = pd.read_csv('data/dataset.csv')
     df = df.sort_values('popularity', ascending=False)
     df = df.head(100)
 
     df = df.parallel_apply(search_genius, axis=1)
 
     df = df.fillna(0)
-    for collumn in ['annotation_count', 'song_id', 'pyongs', 'pageviews', 'lyrics_owner_id']:
+    for collumn in ['annotation_count', 'genius_track_id', 'pyongs', 'pageviews', 'lyrics_owner_id']:
         df[collumn] = df[collumn].astype(int)
 
-    df.to_csv('songs.csv')
+    df.to_csv('data/songs.csv')
